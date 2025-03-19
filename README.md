ca91e2b59e1c3ba669efda8a3368ff7ec6f7f5dd
Coosis <1159727122@qq.com>
2025-03-19 20:55:39 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sofle_rgb_firmware
 Firmware for building sofle rgb keyboard, used with Promicro rp2040 microcontroller.
+Uses some of the original firmware, the rest are from vendor 'PandaKB'.
+
+To use:
+```bash
+qmk flash -kb sofle -km rgb_coosis
+```
---
+++ rules.mk
@@ -0,0 +1,15 @@
+DEFAULT_FOLDER = sofle/rev1
+MCU = RP2040
+BOOTLOADER = rp2040
+SERIAL_DRIVER = vendor
+WS2812_DRIVER = vendor
+
+# CONSOLE_ENABLE = no         # Console for debug
+# COMMAND_ENABLE = no         # Commands for debug and configuration
+# AUDIO_ENABLE = no           # Audio output
+# QMK_SETTINGS    = yes
+# COMBO_ENABLE    = yes
+# KEY_OVERRIDE_ENABLE = yes
+#
+# OLED_DRIVER     = ssd1306
+# WPM_ENABLE      = yes
